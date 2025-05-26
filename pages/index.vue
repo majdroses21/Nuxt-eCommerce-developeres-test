c19930383773028bfa3da242d969cba336f7c835
Majd Ahmed <majdrose@gmail.com>
2025-05-26 05:58:59 +0300

    Add Fonts

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
    <NuxtLayout name="website">
-    Holla Joj
+      Holla Joj
+      <br>
+      <div>
+         <h1 class="font-bold">عنوان رئيسي</h1>
+         <h1 >عنوان رئيسي</h1>
+         <h2 class="font-medium">عنوان فرعي</h2>
+         <h2 >عنوان فرعي</h2>
+         <p class="font-regular">نص عادي</p>
+         <p >نص عادي</p>
+         <span class="font-light">نص خفيف</span> <br>
+         <span>نص خفيف</span>
+      </div>
+      <br>
    </NuxtLayout>
 </template>
